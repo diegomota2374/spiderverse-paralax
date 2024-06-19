572826794c3b4f68e11f72859a4936733a169454
diegomota2374 <diegomota2374@gmail.com>
2024-06-19 13:32:33 -0300

    firt commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: "https://6671b3e8e083e62ee43cc1be.mockapi.io",
+  },
+};
 
 export default nextConfig;
